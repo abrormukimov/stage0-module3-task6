1a37e52df89824044710108a5c26e4c3170b6638
abrormukimov <abrormm34@gmail.com>
2022-09-24 21:41:44 +0500

    Update coding tasks

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float number = 3.32334f;
+        int rounded = Math.round(number);
+        System.out.println(rounded);
     }
 }
